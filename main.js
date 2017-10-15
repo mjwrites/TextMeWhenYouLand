76575be56a84793cacaa6e5aab99cc08dfd6a832
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-14 20:54:54 -0400

    website start

---
+++ main.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.5.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXLWmoF_siEWeAEOZK7SaBBJdJx3A3Ang",
+    authDomain: "textmewhenyouland.firebaseapp.com",
+    databaseURL: "https://textmewhenyouland.firebaseio.com",
+    projectId: "textmewhenyouland",
+    storageBucket: "textmewhenyouland.appspot.com",
+    messagingSenderId: "1049450517098"
+  };
+  firebase.initializeApp(config);
+</script>
